c81e1978bbc0ee80dfa570859fdfb4be345c1476
looxoos <looxoos@gmail.com>
2017-10-13 17:19:48 +0200

    Second step
    Basic code for app !without key reading.

--- run.py
+++ run.py
@@ -1,39 +1,45 @@
 import pygame, sys
 
-max_tps = 70
+class Game(object):
 
-pygame.init()
-screen = pygame.display.set_mode((1280,720))
-box = pygame.Rect(10,10,50,50)
-clock = pygame.time.Clock()
-delta = 0.0
+    def __init__(self):
+        # Configuration
+        self.tps_max = 100.0
 
-while True:
-    #Handling events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit(0)
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            sys.exit(0)
+        # Initialization
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280,720))
+        self.tps_clock = pygame.time.Clock()
+        self.tps_delta = 0.0
 
-    #Ticking
-    delta += clock.tick()/1000.0
-    while delta > 1/max_tps:
-        delta -= 1/max_tps
+        while True:
+            # Handling events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+
+            # Ticking
+            self.tps_delta += self.tps_clock.tick() / 1000.0
+            while self.tps_delta > 1 / self.tps_max:
+                self.tick()
+                self.tps_delta -= 1 / self.tps_max
+
+            # Drawing
+            self.screen.fill((0, 0, 0))
+            self.draw()
+            pygame.display.flip()
+
+
+
+    def tick(self):
+        # Checking inputs
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_d]:
-            box.x += 1
-        if keys[pygame.K_s]:
-            box.y += 1
-        if keys[pygame.K_w]:
-            box.y -= 1
-        if keys[pygame.K_a]:
-            box.x -= 1
 
-    #Input
+    def draw(self):
+        pygame.draw.rect(self.screen, (0, 150, 255), pygame.Rect(10,10,50,50))
 
 
-    #Drawing
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen, (0,150,255), box)
-    pygame.display.flip()
+if __name__ == '__main__':
+    Game()
